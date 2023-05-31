bb42825f16b5b520ce43b00f7fa9dfdb7d9658f6
Greg Kamradt <gkamradt@gmail.com>
2023-05-30 20:07:53 -0700

    parent tweet updates

--- twitter-reply-bot.py
+++ twitter-reply-bot.py
@@ -39,7 +39,7 @@ class TwitterBot:
         self.tweet_response_limit = 35 # How many tweets to respond to each time the program wakes up
 
         # Initialize the language model w/ temperature of .5 to induce some creativity
-        self.llm = ChatOpenAI(temperature=.5, openai_api_key=OPENAI_API_KEY)
+        self.llm = ChatOpenAI(temperature=.5, openai_api_key=OPENAI_API_KEY, model_name='gpt-4')
 
         # For statics tracking for each run. This is not persisted anywhere, just logging
         self.mentions_found = 0
@@ -58,12 +58,11 @@ class TwitterBot:
     
     # Returns the parent tweet text of a mention if it exists. Otherwise returns None
     # We use this to since we want to respond to the parent tweet, not the mention itself
-    def get_mention_parent_tweet(self, referenced_tweets):
-        # There are multiple entries in 'reference_tweets' so we need to find the one that is a 'replied_to' type which is the parent tweet
-        for tweet in referenced_tweets:
-            if tweet.type == 'replied_to':
-                parent_tweet = self.twitter_api.get_tweet(tweet.id).data
-                return parent_tweet
+    def get_mention_conversation_tweet(self, mention):
+        # Check to see if mention has a field 'conversation_id' and if it's not null
+        if mention.conversation_id is not None:
+            conversation_tweet = self.twitter_api.get_tweet(mention.conversation_id).data
+            return conversation_tweet
         return None
 
     # Get mentioned to the user thats authenticated and running the bot.
@@ -74,7 +73,7 @@ class TwitterBot:
         now = datetime.utcnow()
 
         # Subtract 2 hours to get the start time
-        start_time = now - timedelta(hours=2)
+        start_time = now - timedelta(minutes=20)
 
         # Convert to required string format
         start_time_str = start_time.strftime("%Y-%m-%dT%H:%M:%SZ")
@@ -82,36 +81,41 @@ class TwitterBot:
         return self.twitter_api.get_users_mentions(id=self.twitter_me_id,
                                                    start_time=start_time_str,
                                                    expansions=['referenced_tweets.id'],
-                                                   tweet_fields=['created_at']).data
+                                                   tweet_fields=['created_at', 'conversation_id']).data
 
     # Checking to see if we've already responded to a mention with what's logged in airtable
-    def already_responded(self, mention_tweet_id):
+    def already_responded(self, mentioned_conversation_tweet_id):
         records = self.airtable.get_all(view='Grid view')
         for record in records:
-            if record['fields'].get('mentioned_tweet_id') == str(mention_tweet_id):
+            if record['fields'].get('mentioned_conversation_tweet_id') == str(mentioned_conversation_tweet_id):
                 return True
         return False
 
     # Run through all mentioned tweets and generate a response
     def respond_to_mentions(self):
         mentions = self.get_mentions()
+
+        # If no mentions, just return
+        if not mentions:
+            print("No mentions found")
+            return
+        
         self.mentions_found = len(mentions)
 
         for mention in mentions[:self.tweet_response_limit]:
-            # Check to see if there is a parent tweet
-            mentioned_parent_tweet = self.get_mention_parent_tweet(mention.referenced_tweets)
+            # Getting the mention's conversation tweet
+            mentioned_conversation_tweet = self.get_mention_conversation_tweet(mention)
             
-            # If not just move on and don't respond
-            if mentioned_parent_tweet is not None:
-                mentioned_parent_tweet_text = mentioned_parent_tweet.text
-                
-                # Check to see if you've already responded to a mention. If not, then respond
-                if not self.already_responded(mention.id):
-                    self.respond_to_mention(mention, mentioned_parent_tweet_text)
+            # If the mention *is* the conversation or you've already responded, skip it and don't respond
+            if (mentioned_conversation_tweet.id != mention.id
+                and not self.already_responded(mentioned_conversation_tweet.id)):
+
+                self.respond_to_mention(mention, mentioned_conversation_tweet)
+        return True
 
     # Generate a response using the language model
-    def respond_to_mention(self, mention, mentioned_parent_tweet_text):
-        response_text = self.generate_response(mentioned_parent_tweet_text)
+    def respond_to_mention(self, mention, mentioned_conversation_tweet):
+        response_text = self.generate_response(mentioned_conversation_tweet.text)
         
         # Try and create the response to the tweet. If it fails, log it and move on
         try:
@@ -119,22 +123,22 @@ class TwitterBot:
             self.mentions_replied += 1
         except Exception as e:
             print (e)
-            
             self.mentions_replied_errors += 1
             return
         
         # Log the response in airtable if it was successful
         self.airtable.insert({
-            'mentioned_tweet_id': str(mention.id),
-            'mentioned_tweet_parent_text': mentioned_parent_tweet_text,
+            'mentioned_conversation_tweet_id': str(mentioned_conversation_tweet.id),
+            'mentioned_conversation_tweet_text': mentioned_conversation_tweet.text,
             'tweet_response_id': response_tweet.data['id'],
             'tweet_response_text': response_text,
             'tweet_response_created_at' : datetime.utcnow().isoformat(),
             'mentioned_at' : mention.created_at.isoformat()
         })
+        return True
 
     # Generate a response using the language model using the template we reviewed in the jupyter notebook (see README)
-    def generate_response(self, mentioned_parent_tweet_text):
+    def generate_response(self, mentioned_conversation_tweet_text):
         # It would be nice to bring in information about the links, pictures, etc. But out of scope for now
         system_template = """
             You are a smart mad scientist from silicon valley.
@@ -158,7 +162,7 @@ class TwitterBot:
         chat_prompt = ChatPromptTemplate.from_messages([system_message_prompt, human_message_prompt])
 
         # get a chat completion from the formatted messages
-        final_prompt = chat_prompt.format_prompt(text=mentioned_parent_tweet_text).to_messages()
+        final_prompt = chat_prompt.format_prompt(text=mentioned_conversation_tweet_text).to_messages()
         response = self.llm(final_prompt).content
         
         return response
@@ -170,7 +174,8 @@ def job():
     bot.execute_replies()
 
 if __name__ == "__main__":
-    schedule.every(1).minutes.do(job)
+    # Schedule the job to run every 5 minutes. Edit to your liking, but watch out for rate limits
+    schedule.every(5).minutes.do(job)
     while True:
         schedule.run_pending()
         time.sleep(1)
